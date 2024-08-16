79e626f955f2292c1e14755791b4f6e99a098a07
Bryan Lima <bryandslima@gmail.com>
2024-08-16 16:52:16 -0300

    Integração com outras Branchs

---
+++ src/models/varianteModel.js
@@ -0,0 +1,42 @@
+const connection = require ('./connection');
+
+async function modelGetAllVariantes() {
+    const variantes = await connection.query(
+        'SELECT * FROM variante'
+    );
+
+    return variantes.rows;
+}
+
+async function modelCreateVariante(nome) {
+        await connection.query (
+            `INSERT INTO variante (nome)
+            VALUES ('${nome}')`
+            );
+
+        return;
+}
+
+async function modelUpdateVariante(id, nome){
+    await connection.query(`
+        UPDATE variante SET nome = ${nome} WHERE id = ${id}
+    `)
+
+    return;
+}
+
+async function modelDeleteVariante (id) {
+    await connection.query (
+        `DELETE FROM variante
+        WHERE id = ${id}
+        `)
+
+    return;
+}
+
+module.exports = {
+    modelGetAllVariantes,
+    modelCreateVariante,
+    modelUpdateVariante,
+    modelDeleteVariante
+};
